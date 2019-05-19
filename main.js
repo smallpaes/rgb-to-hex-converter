a64f2469e38c999c2364f5a3ec85ef8202259ea1
Mike <mike.huang.mikank@gmail.com>
2019-05-19 14:34:32 +0800

    refactor functions and update styling

--- main.js
+++ main.js
@@ -1,62 +1,48 @@
-const controller = document.getElementById('controller')
-const resultDisplay = document.querySelector('#result-display h1')
-const rgbData = {
-  'red': 0,
-  'green': 0,
-  'blue': 0
-}
-
-//create Converter constructor function
-function Converter() { }
-
-//create instance
-const converter = new Converter()
-
-//create updateBackground method to Converter
-Converter.prototype.updateBackground = function (value) {
-  document.querySelector('body').style.background = value
-}
-
-//create updateRgbNumber method to Converter
-Converter.prototype.updateRgbNumber = function (section, value) {
-  section.textContent = value
-}
-
-Converter.prototype.convertToHex = function (rgbData) {
-  let hexValue = '#'
-  for (let color in rgbData) {
-    hexValue += Number(rgbData[color]).toString(16).padStart(2, 0)
+(function () {
+  const controller = document.getElementById('controller')
+  const resultDisplay = document.querySelector('#result-display h1')
+  const rgbData = {
+    'red': 0,
+    'green': 0,
+    'blue': 0
   }
-  return hexValue
-}
 
-Converter.prototype.updateHexNumber = function (hexValue) {
-  resultDisplay.textContent = hexValue
-}
-
-//display default background color
-converter.updateBackground('#000000')
-
-//add change event listener to controller
-controller.addEventListener('input', event => {
-  if (event.target.tagName === 'INPUT') {
-    const color = event.target.dataset.color
-    const numberSection = event.target.nextElementSibling
-    const numberValue = event.target.value
-
-    //update number value of that input in that section
-    converter.updateRgbNumber(numberSection, numberValue)
-
-    //update number in rgbData object
-    rgbData[color] = numberValue
-
-    //rgb convert to hex number 
-    const hexValue = converter.convertToHex(rgbData)
-
-    //update hex number 
-    converter.updateHexNumber(hexValue)
-
-    //update background color
-    converter.updateBackground(hexValue)
+  function updateBackground(value) {
+    document.querySelector('body').style.background = value
   }
-})
+
+  function convertToHex(rgbData) {
+    let hexValue = '#'
+    for (let color in rgbData) {
+      hexValue += Number(rgbData[color]).toString(16).padStart(2, 0)
+    }
+    return hexValue
+  }
+
+  //display default background color
+  updateBackground('#000000')
+
+  //add input event listener to controller
+  controller.addEventListener('input', event => {
+    if (event.target.tagName === 'INPUT') {
+      const color = event.target.dataset.color
+      const rgbNumberDisplay = event.target.nextElementSibling
+      const numberValue = event.target.value
+
+      //update number value of that input in that section
+      rgbNumberDisplay.textContent = numberValue
+
+      //update number in rgbData object
+      rgbData[color] = numberValue
+
+      //rgb convert to hex number 
+      const hexValue = convertToHex(rgbData)
+
+      //update hex number 
+      resultDisplay.textContent = hexValue
+
+      //update background color
+      updateBackground(hexValue)
+    }
+  })
+})()
